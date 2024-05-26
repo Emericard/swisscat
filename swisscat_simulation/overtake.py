4c0141d25c9b0b9f37e0f5742cd1ae9bf594f4c8
Emericard <Emericard@github.com>
2024-05-26 16:30:36 +0200

    overtake node V1

--- swisscat_simulation/overtake.py
+++ swisscat_simulation/overtake.py
@@ -3,100 +3,61 @@ from numpy import *
 from rclpy.node import Node
 from geometry_msgs.msg import Point, PoseWithCovarianceStamped, Twist
 import math
-from nav_msgs.msg import OccupancyGrid
+import rclpy
+from nav_msgs.msg import OccupancyGrid, Odometry
 
 class OvertakeNode(Node):
     def __init__(self):
         super().__init__('overtake_node')
-        self.robot1_pos_sub = self.create_subscription(Twist, '/Robot1/odom', self.robot1_pos_callback, 10)
-        self.robot2_pos_sub = self.create_subscription(Twist, '/Robot2/odom', self.robot2_pos_callback, 10)
-        self.pose_pub = self.create_publisher(PoseWithCovarianceStamped, '/Robot2/set_pose', 10)
+        self.robot1_pos_sub = self.create_subscription(Odometry, '/Robot1/odom', self.robot1_pos_callback, 10)
+        self.robot2_pos_sub = self.create_subscription(Odometry, '/Robot2/odom', self.robot2_pos_callback, 10)
+        self.pose_pub = self.create_publisher(Twist, '/Robot2/cmd_vel', 10)
         self.occupancy_grid_sub = self.create_subscription(OccupancyGrid, '/map', self.occupancy_grid_callback, 10)
         self.resolution = 0.1  # Example resolution of the occupancy grid (meters per cell)
         self.occupancy_grid_data = None
     
+    pub_ = None
+    regions_ = {
+        'right': 0,
+        'fright': 0,
+        'front': 0,
+        'fleft': 0,
+        'left': 0,
+    }
+    state_ = 0
+    state_dict_ = {
+        0: 'find the wall',
+        1: 'turn left',
+        2: 'follow the wall',
+    }
+
     def robot1_pos_callback(self, msg):
         # Callback function for robot 1 position
-        self.robot1_position = msg
+        self.robot1_position = msg.pose.pose()
             
     def robot2_pos_callback(self, msg):
         # Callback function for robot 2 position
-        self.robot2_position = msg
+        self.robot2_position = msg.pose.pose()
 
     def occupancy_grid_callback(self, msg):
         self.occupancy_grid_data = msg
+
+    def change_state(state):
+        global state_, state_dict_
+        if state is not state_:
+            print('Wall follower - [%s] - %s' % (state, state_dict_[state]))
+            state_ = state
     
-    def calculate_control_command(self):
-        # Assuming position is of type geometry_msgs/Point
-        robot1_x = self.robot1_position.linear.x
-        robot1_y = self.robot1_position.linear.y
-        robot2_x = self.robot2_position.linear.x
-        robot2_y = self.robot2_position.linear.y
-        distance = sqrt((robot1_x-robot2_x)**2+(robot1_y-robot2_y)**2)
-        # Check if robots are close
-        if distance < 500: # Adjust value for robot size
-                # Assuming robot1 is at rest
-                # You might need to subscribe to robot velocities for a more accurate calculation
-                robot1_speed = 1.0  # Speed of robot1 in units per second
-
-                # Calculate time required for robot1 to reach robot2's position
-                time_to_overtake = distance / robot1_speed
-                # You might need additional logic if they have different speeds
-                overtaking_margin = 1.0  # Margin to start overtaking in seconds
-
-                if time_to_overtake < overtaking_margin:
-                    # Generate a control command for robot1 to overtake
-                    self.publish_pose()
-            
-        # If conditions are not met, no overtaking needed
-        return self.publish_pose_test()
-
-    def publish_pose_test(self):
-        robot1_x = self.robot1_position.linear.x
-        robot1_y = self.robot1_position.linear.y
-        if self.occupancy_grid_data is not None:
-            angle_to_wall = self.calculate_distance_to_nearest_wall()[1]
-            pos_x = robot1_x + math.cos(angle_to_wall)
-            pos_y = robot1_y + math.sin(angle_to_wall)
-        pose_msg = PoseWithCovarianceStamped()
-        # Set the header information (frame ID and timestamp)
-        pose_msg.header.frame_id = 'map'  # Frame ID of the pose
-        pose_msg.header.stamp = self.get_clock().now().to_msg()  # Current time
-        # Set the position and orientation of the pose
-        pose_msg.pose.position.x = pos_x  # Example x position
-        pose_msg.pose.position.y = pos_y  # Example y position
-
-        self.publisher_.publish(pose_msg)
-        self.get_logger().info('Publishing pose')
-
-    def publish_pose(self):
-        robot1_x = self.robot1_position.linear.x
-        robot1_y = self.robot1_position.linear.y
-        if self.occupancy_grid_data is not None:
-            angle_to_wall = self.calculate_distance_to_nearest_wall()[1]
-            pos_x = robot1_x + math.cos(angle_to_wall)
-            pos_y = robot1_y + math.sin(angle_to_wall)
-        pose_msg = PoseWithCovarianceStamped()
-        # Set the header information (frame ID and timestamp)
-        pose_msg.header.frame_id = 'map'  # Frame ID of the pose
-        pose_msg.header.stamp = self.get_clock().now().to_msg()  # Current time
-        # Set the position and orientation of the pose
-        pose_msg.pose.position.x = pos_x  # Example x position
-        pose_msg.pose.position.y = pos_y  # Example y position
-
-        self.publisher_.publish(pose_msg)
-        self.get_logger().info('Publishing pose')
-
-
-    def calculate_distance_to_nearest_wall(self):
+    def scan(self):
         # Convert robot's position to grid coordinates
         robot_grid_x = int((self.robot_position.linear.x - self.occupancy_grid_data.info.origin.position.x) / self.resolution)
         robot_grid_y = int((self.robot_position.linear.y - self.occupancy_grid_data.info.origin.position.y) / self.resolution)
 
         # Ray casting to find nearest obstacle
-        min_distance = float('inf')
+        scan = []
+        robot_angle = self.robot1_position.orientation.z()
         for angle in range(0, 360, 5):  # Cast rays in 5 degree increments
-            angle_rad = math.radians(angle)
+            angle_rad = robot_angle + math.radians(angle)
             x_step = math.cos(angle_rad)
             y_step = math.sin(angle_rad)
             x, y = robot_grid_x, robot_grid_y
@@ -107,11 +68,62 @@ class OvertakeNode(Node):
                 distance += 1
                 x += x_step
                 y += y_step
-            min_distance_final = min(min_distance, distance)
-            if min_distance_final == distance :
-                min_distance_angle = angle_rad
+            scan[angle] = distance
+        return scan
+    
+    def regions_action(self, msg):
+        regions = {
+            'right':  min(min(msg.ranges[9:26]), 10),
+            'fright': min(min(msg.ranges[0:17]), 10),
+            'front':  min(min(msg.ranges[-9:9]), 10),
+            'fleft':  min(min(msg.ranges[-17:0]), 10),
+            'left':   min(min(msg.ranges[-26:-9]), 10),
+        }
+        return regions
+                
+            
+    def take_action(self):
+        regions = self.regions_actions(self.scan())
+             
+        d = 1.5
+        
+        if regions['front'] > d and regions['fleft'] > d and regions['fright'] > d:
+            self.change_state(0)
+        elif regions['front'] < d and regions['fleft'] > d and regions['fright'] > d:
+            self.change_state(1)
+        elif regions['front'] > d and regions['fleft'] > d and regions['fright'] < d:
+            self.change_state(2)
+        elif regions['front'] > d and regions['fleft'] < d and regions['fright'] > d:
+            self.change_state(0)
+        elif regions['front'] < d and regions['fleft'] > d and regions['fright'] < d:
+            self.change_state(1)
+        elif regions['front'] < d and regions['fleft'] < d and regions['fright'] > d:
+            self.change_state(1)
+        elif regions['front'] < d and regions['fleft'] < d and regions['fright'] < d:
+            self.change_state(1)
+        elif regions['front'] > d and regions['fleft'] < d and regions['fright'] < d:
+            self.change_state(0)
+        else:
+            rclpy.loginfo(regions)
+    
+    def publish_pose(self) :  
+        rate = rclpy.Rate(20)
+        msg = Twist()
+        while not rclpy.is_shutdown():
+            if state_ == 0: # find wall
+                msg.linear.x = 0.2
+                msg.angular.z = -0.3
+            elif state_ == 1: # turn left
+                msg.angular.z = 0.3
+            elif state_ == 2: # follow wall
+                msg.linear.x = 0.5
+            else:
+                rclpy.logerr('Unknown state!')
+                    
+        rate.sleep()
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing Robot2/Twist')
 
-        return [min_distance * self.resolution, min_distance_angle]
 
 def main(args=None):
     rclpy.init(args=args)
